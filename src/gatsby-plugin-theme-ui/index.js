d7ba05428844cdf0704a3da01b9dd5ba50474917
Nick DeJesus <nick@echobind.com>
2020-09-29 09:41:59 -0400

    more theme-ui things

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,8 +1,18 @@
 export default {
   colors: {
-    text: "#000",
-    background: "#fff",
+    text: '#000',
+    background: '#fff',
     primary: 'teal',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#000',
+        primary: 'teal'
+      }
+    }
+  },
+  body: {
+    mb: [8, 12, 16]
   },
   links: {
     nav: {
@@ -10,8 +20,35 @@ export default {
       transition: '0.5s all ease',
       '&:hover, &:focus': {
         transform: 'scale(1.1)',
-        color: 'white',
+        color: 'white'
       }
+    }
+  },
+  layout: {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  },
+  text: {
+    default: {
+      color: 'text',
+      mb: 5
+    },
+    caps: {
+      textTransform: 'uppercase',
+      letterSpacing: '0.2em'
+    },
+    heading: {
+      fontFamily: 'heading',
+      fontWeight: 'heading',
+      lineHeight: 'heading',
+      textTransform: 'uppercase',
+      color: 'primary',
+      py: 2,
+      my: 2
     }
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,115 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link as GatsbyLink } from 'gatsby'
+import {
+  Box,
+  Container,
+  Grid,
+  Card,
+  Text,
+  Heading,
+  Image,
+  Link,
+  useColorMode,
+  Button
+} from 'theme-ui'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Give us money and we'll send you these fruits for free</h1>
-    <Link to="/products">Click here to see products</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [colorMode, setColorMode] = useColorMode()
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Button
+        onClick={(e) => {
+          setColorMode(colorMode === 'default' ? 'dark' : 'default')
+        }}
+      >
+        Toggle {colorMode === 'default' ? 'Dark' : 'Light'}
+      </Button>
+      <Container>
+        <Heading as="h1">Very Good Fruit Store</Heading>
+
+        <Heading>Features</Heading>
+        <Grid gap={20} columns={[1, 2, 3]}>
+          <Card>
+            <Heading>Fresh</Heading>
+            <Text variant={'text.default'}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+              Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc
+              finibus. Pellentesque eget dapibus velit.
+            </Text>
+          </Card>
+          <Card>
+            <Heading>Tasty</Heading>
+            <Text variant={'text.default'}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+              Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc
+              finibus. Pellentesque eget dapibus velit.
+            </Text>
+          </Card>
+          <Card>
+            <Heading>Organic</Heading>
+            <Text variant={'text.default'}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+              Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc
+              finibus. Pellentesque eget dapibus velit.
+            </Text>
+          </Card>
+        </Grid>
+        <Grid columns={[1, 2]}>
+          <Box>
+            <Image
+              sx={{ backgroundSize: 'contain' }}
+              src={'http://www.fillmurray.com/400/300'}
+            />
+          </Box>
+          <Box>
+            <Heading>This store is great</Heading>
+            <Text variant={'text.default'}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+              Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc
+              finibus. Pellentesque eget dapibus velit. Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Integer tempor urna in orci
+              porttitor, sit amet molestie lorem imperdiet. Cras fermentum arcu
+              nec ante egestas, sit amet rhoncus nunc finibus. Pellentesque eget
+              dapibus velit.
+            </Text>
+          </Box>
+        </Grid>
+        <Grid columns={[1, 2, 2]}>
+          <Box>
+            <Heading>This store is great</Heading>
+            <Text variant={'text.default'}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+              Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc
+              finibus. Pellentesque eget dapibus velit. Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Integer tempor urna in orci
+              porttitor, sit amet molestie lorem imperdiet. Cras fermentum arcu
+              nec ante egestas, sit amet rhoncus nunc finibus. Pellentesque eget
+              dapibus velit.
+            </Text>
+          </Box>
+          <Box>
+            <Image
+              sx={{ backgroundSize: 'contain' }}
+              src={'http://www.fillmurray.com/400/300'}
+            />
+          </Box>
+        </Grid>
+      </Container>
+      <Link as={GatsbyLink} to="/products">
+        Click here to see products
+      </Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
