ea6a442d899ece9d1863a6f6a6f22e1cf7f1672b
Nick DeJesus <nick@echobind.com>
2020-10-01 18:10:40 -0400

    add gatsby prefix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,17 @@
-const React = require("react")
-const { CartProvider } = require("use-shopping-cart")
-const { loadStripe } = require("@stripe/stripe-js")
+const React = require('react')
+const { CartProvider } = require('use-shopping-cart')
+const { loadStripe } = require('@stripe/stripe-js')
 
-const stripePromise = loadStripe(process.env.STRIPE_API_PUBLIC)
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_API_PUBLIC)
 
 export const wrapRootElement = ({ element }) => {
   return (
     <CartProvider
       stripe={stripePromise}
-      successUrl={"https://twitter.com/dayhaysoos"}
-      cancelUrl={"https://twitter.com/dayhaysoos"}
+      successUrl={'https://twitter.com/dayhaysoos'}
+      cancelUrl={'https://twitter.com/dayhaysoos'}
       currency="USD"
-      allowedCountries={["US", "GB", "CA"]}
+      allowedCountries={['US', 'GB', 'CA']}
       billingAddressCollection={true}
       mode="checkout-session"
     >
