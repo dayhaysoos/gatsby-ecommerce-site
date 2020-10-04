3fcdd044bf4aab30544efac6afbb1eaba6f21a8f
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 16:47:31 -0400

    remove image component, add theme-ui image, do more grid things

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Container, Heading, Grid, Card, Text } from 'theme-ui'
+import { Container, Heading, Grid, Card, Text, Box, Image } from 'theme-ui'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
@@ -39,6 +38,48 @@ const IndexPage = () => (
             Pellentesque eget dapibus velit.
           </Text>
         </Card>
+      </Grid>
+      <Grid columns={[1, 2]}>
+        <Box>
+          <Image
+            sx={{ backgroundSize: 'contain' }}
+            src={'https://www.fillmurray.com/400/300'}
+          />
+        </Box>
+        <Box>
+          <Heading>This store is great</Heading>
+          <Text variant={'text.default'}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Integer tempor urna in orci porttitor,
+            sit amet molestie lorem imperdiet. Cras fermentum arcu nec ante
+            egestas, sit amet rhoncus nunc finibus. Pellentesque eget dapibus
+            velit.
+          </Text>
+        </Box>
+      </Grid>
+      <Grid columns={[1, 2, 2]}>
+        <Box>
+          <Heading>This store is great</Heading>
+          <Text variant={'text.default'}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Integer tempor urna in orci porttitor,
+            sit amet molestie lorem imperdiet. Cras fermentum arcu nec ante
+            egestas, sit amet rhoncus nunc finibus. Pellentesque eget dapibus
+            velit.
+          </Text>
+        </Box>
+        <Box>
+          <Image
+            sx={{ backgroundSize: 'contain' }}
+            src={'https://www.fillmurray.com/400/300'}
+          />
+        </Box>
       </Grid>
     </Container>
   </Layout>
