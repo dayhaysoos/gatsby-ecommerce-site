bab01d0d82a288b043124a973e8d541f33689fa2
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 16:52:56 -0400

    add dark color mode

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,7 +2,14 @@ export default {
   colors: {
     text: '#000',
     background: '#fff',
-    primary: 'teal'
+    primary: 'teal',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#000',
+        primary: 'teal'
+      }
+    }
   },
   fonts: {
     body:
