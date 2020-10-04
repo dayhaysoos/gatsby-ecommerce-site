b8741a764ff698059ab62e307521cc1055c0402b
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 16:21:52 -0400

    add theme-ui object

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,13 +2,6 @@ export default {
   colors: {
     text: '#000',
     background: '#fff',
-    primary: 'teal',
-    modes: {
-      dark: {
-        text: '#fff',
-        background: '#000',
-        primary: 'teal'
-      }
-    }
+    primary: 'teal'
   }
 }
