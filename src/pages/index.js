ae3556310040ec1f318092a2bb11006d5dc11c94
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 16:13:41 -0400

    add theme-ui Container and Heading components to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,17 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
+import { Container, Heading } from 'theme-ui'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Container>
+      <Heading as="h1">Very Good Fruit Store</Heading>
+    </Container>
   </Layout>
 )
 
