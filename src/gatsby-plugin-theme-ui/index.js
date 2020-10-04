be0927785914d5144394fa1bfc63e6ecf327dc24
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 16:22:15 -0400

    add fonts and font  weights to theme object

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -3,5 +3,16 @@ export default {
     text: '#000',
     background: '#fff',
     primary: 'teal'
+  },
+  fonts: {
+    body:
+      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'Georgia, serif',
+    monospace: 'Menlo, monospace'
+  },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700
   }
 }
