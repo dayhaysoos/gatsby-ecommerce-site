86e7c44ec308cfba0e377802fae8acae919b11e4
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 16:00:40 -0400

    create gatsby-plugin-theme-ui folder and index.js and add colors object

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: 'teal',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#000',
+        primary: 'teal'
+      }
+    }
+  }
+}
