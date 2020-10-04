9fd55672a80530c3be3f2fc7a5c60faa72f75b27
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 16:44:40 -0400

    add more theme-ui components with resonsive grid array

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Container, Heading } from 'theme-ui'
+import { Container, Heading, Grid, Card, Text } from 'theme-ui'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -11,6 +11,35 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Container>
       <Heading as="h1">Very Good Fruit Store</Heading>
+      <Grid gap={20} columns={[1, 2, 3]}>
+        <Card>
+          <Heading>Fresh</Heading>
+          <Text variant={'text.default'}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit.
+          </Text>
+        </Card>
+        <Card>
+          <Heading>Tasty</Heading>
+          <Text variant={'text.default'}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit.
+          </Text>
+        </Card>
+        <Card>
+          <Heading>Organic</Heading>
+          <Text variant={'text.default'}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit.
+          </Text>
+        </Card>
+      </Grid>
     </Container>
   </Layout>
 )
