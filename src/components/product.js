67650bda5f374f01fd179a2eeb7fe1e04d9f0943
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 22:56:47 -0400

    import theme-ui components and useShoppingCart, destructure product object

--- src/components/product.js
+++ src/components/product.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Box, Button, Grid, Image } from 'theme-ui'
+import { useShoppingCart, formatCurrencyString } from 'use-shopping-cart'
 
 function Product({ product }) {
-  console.log('product', product)
+  const { name, image, currency, description, price, tags } = product
+  const { addItem } = useShoppingCart()
+
   return (
     <>
       <h1>This is a product component</h1>
