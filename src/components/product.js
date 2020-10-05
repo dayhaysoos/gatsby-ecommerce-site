d65d6b0a0157e2a0d1a4b626cd4bca5db1bf8502
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 23:06:29 -0400

    build out the product component

--- src/components/product.js
+++ src/components/product.js
@@ -10,6 +10,39 @@ function Product({ product }) {
   return (
     <Box sx={{ maxWidth: '768px', margin: '0 auto' }}>
       <SEO title={name} />
+      <Grid sx={{ color: 'primary', fontWeight: '700' }} columns={[1, 2]}>
+        <Box>
+          <Image alt={`${name}`} src={image} />
+        </Box>
+        <Box
+          as="section"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+          as="section"
+        >
+          <Box
+            sx={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'column'
+            }}
+          >
+            <Box as="p">{description}</Box>
+            <Box as="p">{formatCurrencyString({ value: price, currency })}</Box>
+          </Box>
+          <Button
+            onClick={() => addItem(product)}
+            sx={{ backgroundColor: 'teal' }}
+            alt={`Add ${name} to cart`}
+          >
+            Add to Cart
+          </Button>
+        </Box>
+      </Grid>
     </Box>
   )
 }
