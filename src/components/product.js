ddcf85dc104555f6e1d3544bb98acece40e3e43a
Nick DeJesus <ndejesus1227@gmail.com>
2020-10-04 22:53:09 -0400

    log data to make sure it's getting passed from template to component

--- src/components/product.js
+++ src/components/product.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-function Product() {
-  return <h1>This is a product component</h1>
+function Product({ product }) {
+  console.log('product', product)
+  return (
+    <>
+      <h1>This is a product component</h1>
+    </>
+  )
 }
 
 export default Product
