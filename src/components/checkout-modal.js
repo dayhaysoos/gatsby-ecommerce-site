b3428badd4fe8c1fd51fe92fdd705168cc4cdf91
Nick DeJesus <nick@echobind.com>
2020-10-05 22:37:30 -0400

    add jsx pragma and import components from react ui

---
+++ src/components/checkout-modal.js
@@ -0,0 +1,6 @@
+/**@jsx jsx */
+import { jsx } from 'theme-ui'
+import { useShoppingCart } from 'use-shopping-cart'
+import CheckoutDetails from './checkout-details'
+import { DialogOverlay, DialogContent } from '@reach/dialog'
+import '@reach/dialog/styles.css'
