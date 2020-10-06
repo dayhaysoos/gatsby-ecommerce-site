91813ee1cd8fc74d619842a4c26f42615557b17a
Nick DeJesus <nick@echobind.com>
2020-10-05 21:39:04 -0400

    add gql query to access all products on the product page

--- src/pages/products.js
+++ src/pages/products.js
@@ -2,8 +2,26 @@ import React from 'react'
 import { Box, Button, Grid, Image } from 'theme-ui'
 import Layout from '../components/layout'
 import { useShoppingCart, formatCurrencyString } from 'use-shopping-cart'
+import { graphql, Link } from 'gatsby'
 
-function Products() {
+export const query = graphql`
+  {
+    allProduct {
+      nodes {
+        slug
+        currency
+        description
+        id
+        image
+        name
+        price
+        sku
+      }
+    }
+  }
+`
+
+function Products({ data }) {
   return (
     <Layout>
       <h1>Produts page</h1>
