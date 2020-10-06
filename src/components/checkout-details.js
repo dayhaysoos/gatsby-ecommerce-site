92220abd6afa2b26a33d920f50dc7a622f0f9afd
Nick DeJesus <nick@echobind.com>
2020-10-06 00:01:24 -0400

    display basic content

--- src/components/checkout-details.js
+++ src/components/checkout-details.js
@@ -12,8 +12,52 @@ function CheckoutDetails() {
   } = useShoppingCart()
 
   return (
-    <Box>
-      <h2>Checkout details</h2>
+    <Box as="form">
+      <Box sx={{ marginBottom: '24px' }} as="table">
+        <caption className={`visually-hidden`}>Shopping Cart</caption>
+        <Box as="tr">
+          <Box as="th" scope="col">
+            Name
+          </Box>
+          <Box as="th" scope="col">
+            Description
+          </Box>
+          <Box as="th" scope="col">
+            Total
+          </Box>
+          <Box as="th" scope="col">
+            Quantity
+          </Box>
+        </Box>
+        <Box as="tbody">
+          {Object.keys(cartDetails).map((cartItem) => {
+            const item = cartDetails[cartItem]
+            return (
+              <Box as="tr" key={item.sku}>
+                <Box as="td" scope="row">
+                  {item.name}
+                </Box>
+                <Box as="td">{item.description}</Box>
+                <Box as="td">{item.formattedValue}</Box>
+                <Box as="td">
+                  <label>
+                    <span className={`visually-hidden`}>
+                      Quantity of {item.name}
+                    </span>
+                  </label>
+                </Box>
+                <Box as="td">
+                  <Close
+                    aria-label={`Remove ${item.name} from cart`}
+                    title={'Remove'}
+                    onClick={() => removeItem(item.sku)}
+                  />
+                </Box>
+              </Box>
+            )
+          })}
+        </Box>
+      </Box>
     </Box>
   )
 }
