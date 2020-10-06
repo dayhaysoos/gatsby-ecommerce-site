4fbfccd66a56dfe83b0c54b3235155768afa6cd6
Nick DeJesus <nick@echobind.com>
2020-10-05 22:58:33 -0400

    create checkout details component

---
+++ src/components/checkout-details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useShoppingCart } from 'use-shopping-cart'
+import { Box, Flex, Button, Close } from 'theme-ui'
+
+function CheckoutDetails() {
+  return (
+    <Box>
+      <h2>Checkout details</h2>
+    </Box>
+  )
+}
+
+export default CheckoutDetails
