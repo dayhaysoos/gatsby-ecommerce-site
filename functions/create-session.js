0611cb706412ee51c32b10c28b04e031e20e4bbe
Nick DeJesus <nick@echobind.com>
2020-10-06 16:50:40 -0400

    fix typo

--- functions/create-session.js
+++ functions/create-session.js
@@ -22,7 +22,7 @@ exports.handler = async (event) => {
     })
 
     return {
-      satusCode: 200,
+      statusCode: 200,
       body: JSON.stringify({ sessionId: session.id })
     }
   } catch (error) {
