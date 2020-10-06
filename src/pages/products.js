0f53d3dccc46d5fa9d107c93a75e1c63633df9ac
Nick DeJesus <nick@echobind.com>
2020-10-05 21:32:02 -0400

    wrap layout around products page

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Box, Button, Grid, Image } from 'theme-ui'
+import Layout from '../components/layout'
+import { useShoppingCart, formatCurrencyString } from 'use-shopping-cart'
 
 function Products() {
-  return <h1>Products Page</h1>
+  return (
+    <Layout>
+      <h1>Produts page</h1>
+    </Layout>
+  )
 }
 
 export default Products
