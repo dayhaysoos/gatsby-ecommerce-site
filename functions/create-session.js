5bfdd45d90cb2fe829bd3206331890e6e9562096
Nick DeJesus <nick@echobind.com>
2020-10-06 14:14:03 -0400

    add handler for validating items and creating sessionId

--- functions/create-session.js
+++ functions/create-session.js
@@ -3,3 +3,29 @@ const validateCartItems = require('use-shopping-cart/src/serverUtil')
   .validateCartItems
 
 const inventory = require('../data/products.json')
+
+exports.handler = async (event) => {
+  try {
+    const productJSON = JSON.parse(event.body)
+
+    const line_items = validateCartItems(inventory, productJSON)
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      billing_address_collection: 'auto',
+      shipping_address_collection: {
+        allowed_countries: ['US', 'CA']
+      },
+      success_url: `${process.env.URL}/success.html`,
+      cancel_url: process.env.URL,
+      line_items
+    })
+
+    return {
+      satusCode: 200,
+      body: JSON.stringify({ sessionId: session.id })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
