6bade8ff426a83506f394d42018f5c2558afe867
Nick DeJesus <nick@echobind.com>
2020-10-05 22:39:15 -0400

    set up checkout modal

--- src/components/checkout-modal.js
+++ src/components/checkout-modal.js
@@ -1,6 +1,26 @@
 /**@jsx jsx */
 import { jsx } from 'theme-ui'
 import { useShoppingCart } from 'use-shopping-cart'
-import CheckoutDetails from './checkout-details'
 import { DialogOverlay, DialogContent } from '@reach/dialog'
 import '@reach/dialog/styles.css'
+
+function CheckoutModal() {
+  const { shouldDisplayCart, handleCloseCart, cartCount } = useShoppingCart()
+  return (
+    <DialogOverlay onDismiss={handleCloseCart} isOpen={shouldDisplayCart}>
+      <DialogContent
+        sx={{ backgroundColor: 'background', width: ['100vw', '90vw'] }}
+      >
+        {cartCount === 0 ? (
+          <h2 sx={{ textAlign: 'center', backgroundColor: 'background' }}>
+            No items in cart
+          </h2>
+        ) : (
+          <h2>Checkout details here</h2>
+        )}
+      </DialogContent>
+    </DialogOverlay>
+  )
+}
+
+export default CheckoutModal
