87411badbcb280689c380ef15daf0db264be59c9
Nick DeJesus <nick@echobind.com>
2020-10-06 13:48:15 -0400

    create select component

---
+++ src/components/select.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useShoppingCart } from 'use-shopping-cart'
+
+function Select({ max = 10, cartItem }) {
+  const { setItemQuantity } = useShoppingCart()
+  const options = []
+  for (let quantity = 1; quantity <= max; ++quantity)
+    options.push(
+      <option key={quantity} value={quantity}>
+        {quantity}
+      </option>
+    )
+
+  return (
+    <select
+      value={cartItem.quantity}
+      fontSize={'20px'}
+      onChange={(event) => {
+        setItemQuantity(cartItem.sku, event.target.value)
+      }}
+    >
+      {options}
+    </select>
+  )
+}
+
+export default Select
