92b228eed2d607803a7c9dbd4cbe38acd94a7cb0
Nick DeJesus <nick@echobind.com>
2020-10-05 23:47:04 -0400

    destruct from useShoppingCart()

--- src/components/checkout-details.js
+++ src/components/checkout-details.js
@@ -3,6 +3,14 @@ import { useShoppingCart } from 'use-shopping-cart'
 import { Box, Flex, Button, Close } from 'theme-ui'
 
 function CheckoutDetails() {
+  const {
+    cartDetails,
+    formattedTotalPrice,
+    handleCartClick,
+    removeItem,
+    redirectToCheckout
+  } = useShoppingCart()
+
   return (
     <Box>
       <h2>Checkout details</h2>
